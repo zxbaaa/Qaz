912429d7c144e96c26886c0bc620601205c3d483
zxbaaa <1293937531@qq.com>
2017-12-13 05:41:32 +0800

    第一次

---
+++ src/Aaa/Zzz.java
@@ -0,0 +1,26 @@
+package Aaa;
+
+import java.util.Scanner;
+
+public class Zzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner sc=new Scanner(System.in);
+  System.out.println("请输入一个数");
+  int a=sc.nextInt();
+  int[] arr=new int[a];
+  System.out.println(arr.length);
+ 
+  }
+public static void For(int a){
+	for (int i = 0; i < arr.length; i++) {
+		
+	}
+}
+	
+	
+	
+	
+	}
+
